c2c9e1496382ad548021e6f433a5f2688225bc07
Md. Zayed Hasan <72243831+Iamzayed007@users.noreply.github.com>
2023-09-29 20:45:41 +0600

    problem 1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,63 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
-
+    const [loading, setLoading] = useState(false);
+    const [inputData ,setInputData]= useState({})
+    const [allData ,setAllData]= useState([])
+    const [filterData ,setFilterData]= useState(allData)
+    // let data =[]
     const handleClick = (val) =>{
         setShow(val);
     }
+    const handleName = (e) =>{
+       setInputData({
+        ...inputData,
+        name:e.target.value
+       })
+    }
+    const handleStatus = (e) =>{
+       setInputData({
+        ...inputData,
+        status:e.target.value
+       })
+    }
+
+    const handleSubmit = (data,e) =>{
+        e.preventDefault()
+        // setShow(val);
+        const newData = data
+        // allData.push(data)
+        setAllData([...allData, newData]);
+    }
+    useEffect(()=>{
+        if(show === 'active'){
+            const filter = allData.filter(dt=>dt.status == 'active')
+            setFilterData(filter)
+        }
+       else if(show === 'completed'){
+            const filter = allData.filter(dt=>dt.status == 'completed')
+            setFilterData(filter)
+        }
+        else{
+            const sortedData = [...allData].sort((a, b) => {
+                if (a.status === 'active' && b.status !== 'active') {
+                    return -1;
+                  } else if (a.status === 'completed' && b.status !== 'active' && b.status !== 'completed') {
+                    return -1;
+                  } else if (a.status !== 'active' && a.status !== 'completed' && b.status === 'active') {
+                    return 1;
+                  } else if (a.status !== 'active' && a.status !== 'completed' && b.status === 'completed') {
+                    return 1;
+                  } else {
+                    return 0;
+                  }
+                
+              });
+            setFilterData(sortedData)
+        }
+    },[show,allData])
 
     return (
 
@@ -14,12 +65,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form  onSubmit={(e)=>handleSubmit(inputData,e)} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" onChange={handleName} className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" onChange={handleStatus} className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -47,7 +98,9 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                        { !loading &&
+                           <ChildContent allData={filterData}/>
+                        }
                         </tbody>
                     </table>
                 </div>
@@ -57,3 +110,12 @@ const Problem1 = () => {
 };
 
 export default Problem1;
+
+const ChildContent =(props)=>{
+return(
+    props.allData.map(dt=><tr>
+        <td>{dt.name}</td>
+        <td>{dt.status}</td>
+        </tr>)
+)
+}
